309a3b40365790ffa6fd49424b108759b75c123c
Marco Marchini <775639+marcom04@users.noreply.github.com>
2025-08-28 20:30:12 +0200

    Add neovim-tree

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ require('lazy').setup({
   require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.neo-tree',
 
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
