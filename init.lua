4950741fd8812e214ab06c39ba83b3df341f4e35
Marco Marchini <775639+marcom04@users.noreply.github.com>
2025-08-31 23:00:49 +0200

    Add python LSP, linter, formatter.

--- init.lua
+++ init.lua
@@ -98,10 +98,10 @@ require('lazy').setup({
   -- Highlight, edit, and navigate code
   require 'custom.plugins.treesitter',
 
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
 
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
